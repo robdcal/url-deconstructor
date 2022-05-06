cf62348e506459f68745f07923e81364c9af80ff
Rob Callaghan <rob_cal_uk@hotmail.com>
2022-05-07 00:00:46 +0100

    conditionally show table

--- src/components/UrlParts.jsx
+++ src/components/UrlParts.jsx
@@ -5,44 +5,47 @@ const UrlParts = ({ deconstructedUrl }) => {
 
   return (
     <Fragment>
-      <table>
-        <tbody>
-          <tr>
-            <th colSpan={2}>
-              <strong>Full URL</strong>
-            </th>
-          </tr>
-          <tr>
-            <td>Hostname</td>
-            <td>{hostname}</td>
-          </tr>
-          <tr>
-            <td>Pathname</td>
-            <td>{pathname}</td>
-          </tr>
-          <tr>
-            <td>Hash</td>
-            <td>{hash}</td>
-          </tr>
-          <tr>
-            <td>Search</td>
-            <td>{search}</td>
-          </tr>
-          <tr>
-            <th colSpan={2}>
-              <strong>Query String Keys & Values</strong>
-            </th>
-          </tr>
-          {Array.from(searchParams.entries()).map((item, index) => {
-            return (
-              <tr key={index}>
-                <td>{item[0]}</td>
-                <td>{item[1]}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {/* {console.log(deconstructedUrl.hostname ? true : false)} */}
+      {deconstructedUrl.hostname ? (
+        <table>
+          <tbody>
+            <tr>
+              <th colSpan={2}>
+                <strong>Full URL</strong>
+              </th>
+            </tr>
+            <tr>
+              <td>Hostname</td>
+              <td>{hostname}</td>
+            </tr>
+            <tr>
+              <td>Pathname</td>
+              <td>{pathname}</td>
+            </tr>
+            <tr>
+              <td>Hash</td>
+              <td>{hash}</td>
+            </tr>
+            <tr>
+              <td>Search</td>
+              <td>{search}</td>
+            </tr>
+            <tr>
+              <th colSpan={2}>
+                <strong>Query String Keys & Values</strong>
+              </th>
+            </tr>
+            {Array.from(searchParams.entries()).map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{item[0]}</td>
+                  <td>{item[1]}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      ) : null}
     </Fragment>
   );
 };
