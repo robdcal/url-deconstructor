61dc1bdcdafe7aa634b0288e2b2947ebb23e4bfa
Rob Callaghan <rob_cal_uk@hotmail.com>
2022-05-06 21:57:09 +0100

    implement query string deconstruction

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const deconstructUrl = (prevState, action) => {
     pathname: url.pathname ? url.pathname : null,
     hash: url.hash ? url.hash : null,
     search: url.search ? url.search : null,
-    searchParams: url.searchParams ? url.searchParams : null,
+    searchParams: url.searchParams ? url.searchParams : [],
   };
 };
 
@@ -18,10 +18,13 @@ const App = () => {
   const [Url, setUrl] = useState(
     "https://id.matillion.com/login?state=hKFo2SBtaVVBNTJ2bERUNDREc0NWLVE2T2J6U0ZNQVdPYjE1baFupWxvZ2luo3RpZNkgWFhtX25hUktmSER5N0ptVmVXbmZISEw2bDVDQ3BOVVKjY2lk2SB4dFU3MXhkc0w2TVhFZjVsenBPRHZkRkd2WngwMW9mTw&client=xtU71xdsL6MXEf5lzpODvdFGvZx01ofO&protocol=oauth2&audience=https%3A%2F%2Fapi.matillion.com&redirect_uri=https%3A%2F%2Fhub.matillion.com%3FreturnTo%3DeyJvcmlnaW4iOiJodHRwczovL2RhdGFsb2FkZXIubWF0aWxsaW9uLmNvbSIsInBhdGhuYW1lIjoiL3JlZ2lzdGVyIiwic2VhcmNoIjoiIn0%3D&pageBranding=mdl-default&scope=openid%20profile%20email&response_type=code&response_mode=query&nonce=emdIfmVZUjNWMTZ%2Ba0JZTmNJYklQX2FfVFRkMm9hUUhwWW90OUtCMFh2WQ%3D%3D&code_challenge=b_I1Nb_AxOUygfrE0bnArWvir0TELLeqFNBfRdn4j4E&code_challenge_method=S256&auth0Client=eyJuYW1lIjoiYXV0aDAtcmVhY3QiLCJ2ZXJzaW9uIjoiMS41LjAifQ%3D%3D"
   );
-  const [deconstructedUrl, dispatchDeconstructor] = useReducer(
-    deconstructUrl,
-    {}
-  );
+  const [deconstructedUrl, dispatchDeconstructor] = useReducer(deconstructUrl, {
+    hostname: null,
+    pathname: null,
+    hash: null,
+    search: null,
+    searchParams: [],
+  });
 
   const handleChange = (event) => {
     setUrl(event.target.value);
--- src/components/UrlParts.jsx
+++ src/components/UrlParts.jsx
@@ -1,17 +1,48 @@
 import { Fragment } from "react";
-import UrlPart from "./UrlPart";
 
 const UrlParts = ({ deconstructedUrl }) => {
   const { hostname, pathname, hash, search, searchParams } = deconstructedUrl;
 
   return (
     <Fragment>
-      {/* <UrlPart label={"Protocol"}></UrlPart>
-      <UrlPart label={"Subdomain"}></UrlPart>
-      <UrlPart label={"Domain"}></UrlPart> */}
-      <UrlPart label={"Hostname"} value={hostname}></UrlPart>
-      <UrlPart label={"Pathname"} value={pathname}></UrlPart>
-      <UrlPart label={"Hash"} value={hash}></UrlPart>
+      <table>
+        <tbody>
+          <tr>
+            <td colSpan={2}>
+              <strong>Full URL</strong>
+            </td>
+          </tr>
+          <tr>
+            <td>Hostname</td>
+            <td>{hostname}</td>
+          </tr>
+          <tr>
+            <td>Pathname</td>
+            <td>{pathname}</td>
+          </tr>
+          <tr>
+            <td>Hash</td>
+            <td>{hash}</td>
+          </tr>
+          <tr>
+            <td>Search</td>
+            <td>{search}</td>
+          </tr>
+          <tr>
+            <td colSpan={2}>
+              <strong>Query String Keys & Values</strong>
+            </td>
+          </tr>
+          {Array.from(searchParams.entries()).map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{item[0]}</td>
+                <td>{item[1]}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </Fragment>
   );
 };
