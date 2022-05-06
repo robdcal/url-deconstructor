b8a329df209e954f049c9c7f6260ebab63e76268
Rob Callaghan <rob_cal_uk@hotmail.com>
2022-05-07 00:00:30 +0100

    class name & text update

--- src/components/UrlInput.jsx
+++ src/components/UrlInput.jsx
@@ -1,7 +1,7 @@
 function UrlInput({ handleSubmit, inputValue, handleChange }) {
   return (
-    <div className="UrlInput">
-      <h2>URL Input</h2>
+    <div className="urlInput">
+      <h2>Enter your URL</h2>
       <form action="submit" onSubmit={handleSubmit}>
         <input type="text" value={inputValue} onChange={handleChange} />
         <button>Deconstruct</button>
