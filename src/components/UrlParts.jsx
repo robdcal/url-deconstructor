219cc3ef8ccb71cedcc36a2cd64cbee1b2f4e1d0
Rob Callaghan <rob_cal_uk@hotmail.com>
2022-05-08 08:20:43 +0100

    conditionally check for each part

--- src/components/UrlParts.jsx
+++ src/components/UrlParts.jsx
@@ -5,46 +5,53 @@ const UrlParts = ({ deconstructedUrl }) => {
 
   return (
     <Fragment>
-      {/* {console.log(deconstructedUrl.hostname ? true : false)} */}
       {deconstructedUrl.hostname ? (
-        <table>
-          <tbody>
-            <tr>
-              <th colSpan={2}>
-                <strong>Full URL</strong>
-              </th>
-            </tr>
-            <tr>
-              <td>Hostname</td>
-              <td>{hostname}</td>
-            </tr>
-            <tr>
-              <td>Pathname</td>
-              <td>{pathname}</td>
-            </tr>
-            <tr>
-              <td>Hash</td>
-              <td>{hash}</td>
-            </tr>
-            <tr>
-              <td>Search</td>
-              <td>{search}</td>
-            </tr>
-            <tr>
-              <th colSpan={2}>
-                <strong>Query String Keys & Values</strong>
-              </th>
-            </tr>
-            {Array.from(searchParams.entries()).map((item, index) => {
-              return (
-                <tr key={index}>
-                  <td>{item[0]}</td>
-                  <td>{item[1]}</td>
+        <Fragment>
+          <h3>Full URL Breakdown</h3>
+          <table>
+            <tbody>
+              <tr>
+                <td>Hostname</td>
+                <td>{hostname}</td>
+              </tr>
+              {pathname ? (
+                <tr>
+                  <td>Pathname</td>
+                  <td>{pathname}</td>
                 </tr>
-              );
-            })}
-          </tbody>
-        </table>
+              ) : null}
+              {hash ? (
+                <tr>
+                  <td>Hash</td>
+                  <td>{hash}</td>
+                </tr>
+              ) : null}
+              {search ? (
+                <tr>
+                  <td>Search</td>
+                  <td>{search}</td>
+                </tr>
+              ) : null}
+            </tbody>
+          </table>
+        </Fragment>
+      ) : null}
+      {deconstructedUrl.search ? (
+        <Fragment>
+          <h3>Query String Keys & Values</h3>
+          <table>
+            <tbody>
+              {Array.from(searchParams.entries()).map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{item[0]}</td>
+                    <td>{item[1]}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </Fragment>
       ) : null}
     </Fragment>
   );
